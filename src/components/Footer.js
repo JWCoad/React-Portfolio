34d7e2efe4ac6951137d624643bddc56a2e740c1
Jesse Coad <jessecoad@gmail.com>
2021-09-05 17:37:18 +1000

    deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,7 @@ function Footer() {
       <Container fluid={true}>
         <Row className="border-top justify-content-between p-3">
           <Col className="p-0" md={3} sm={12}>
-            Created by
-          </Col>
-          <Col className="p-0 d-flex justify-content-end" md={3}>
-            JWC
+            Created by JWC
           </Col>
         </Row>
       </Container>
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,7 +8,18 @@ function AboutPage(props) {
       <Hero title={props.title} />
 
       <Content>
-        <p>test</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          lacinia quis libero sit amet accumsan. Curabitur tempor ornare erat,
+          nec commodo lectus pellentesque vitae. Duis justo mauris, sodales vel
+          urna sit amet, molestie tempus leo. Donec iaculis dolor sed felis
+          porttitor varius. Duis gravida nisi nec quam lacinia tincidunt. Etiam
+          id ullamcorper urna. Aliquam ornare consequat interdum. Quisque ornare
+          est et molestie laoreet. Vestibulum congue in ex in faucibus.
+          Suspendisse interdum massa hendrerit ultrices placerat. Aliquam erat
+          volutpat. Etiam consectetur facilisis lectus, sit amet dictum ex
+          auctor a.
+        </p>
       </Content>
     </div>
   );
